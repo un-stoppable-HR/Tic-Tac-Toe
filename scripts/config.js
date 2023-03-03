ceb2e36777986e80dbd9f6f64106a42352b733a8
Harsh Rajput <iamharshrajput@gmail.com>
2023-03-03 14:22:01 +0530

    Used JavaScript to handle form submission & added validation to the user input

--- scripts/config.js
+++ scripts/config.js
@@ -6,4 +6,19 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("player-name").trim();
+
+  if (!enteredPlayerName) {
+    // equivalent to enteredPlayerName === ""
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name !";
+    return;
+  }
 }
