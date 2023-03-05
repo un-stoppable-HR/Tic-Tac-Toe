6c94bfad4250d873b1c365d81e08dc6ff82fb274
Harsh Rajput <iamharshrajput@gmail.com>
2023-03-05 21:51:11 +0530

    Added end game and restart logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,32 @@
+function resetGameStatus() {
+  gameIsOver = false;
+  activePlayer = 0;
+  currentRound = 1;
+  gameOverElement.firstElementChild.innerHTML =
+    'You Won, <span id="winner-name">PLAYER NAME</span> !';
+  gameOverElement.style.display = "none";
+
+  let gameBoardIndex = 0;
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = "";
+      gameBoardItemElement.classList.remove("disabled");
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
   if (players[0].name === "" || players[1].name === "") {
     alert("Please set custom player names for both players!");
     return;
   }
+
+  resetGameStatus();
+
   activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
 }
@@ -18,7 +42,7 @@ function switchPayer() {
 }
 
 function selectGameField(event) {
-  if (event.target.tagName != "LI") {
+  if (event.target.tagName != "LI" || gameIsOver) {
     return;
   }
 
@@ -38,7 +62,10 @@ function selectGameField(event) {
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
   const winnerId = checkForGameOver();
-  console.log(winnerId);
+
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
 
   currentRound++;
   switchPayer();
@@ -91,4 +118,17 @@ function checkForGameOver() {
   }
 
   return 0;
+}
+
+function endGame(winnerId) {
+  gameIsOver = true;
+  gameOverElement.style.display = "block";
+
+  if (winnerId > 0) {
+    const winnerName = players[winnerId - 1].name;
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      winnerName;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a DRAW!";
+  }
 }
