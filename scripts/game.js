d3130adcb75a7912db7ccc635f1f92c84990e988
Harsh Rajput <iamharshrajput@gmail.com>
2023-03-05 20:32:29 +0530

    Managed Game Rounds (Turns) & Field selection

--- scripts/game.js
+++ scripts/game.js
@@ -3,5 +3,25 @@ function startNewGame() {
     alert("Please set custom player names for both players!");
     return;
   }
+  activePlayerNameElement.textContent = players[activePlayer].name;
   gameAreaElement.style.display = "block";
+}
+
+function switchPayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  if (event.target.tagName != "LI") {
+    return;
+  }
+  event.target.textContent = players[activePlayer].symbol;
+  event.target.classList.add("disabled");
+  switchPayer();
 }
