dbdda7a518d90216c50e8a9d57cf5dd431f27daf
Harsh Rajput <iamharshrajput@gmail.com>
2023-03-05 21:21:30 +0530

    Checked for a Winner or Draw

--- scripts/game.js
+++ scripts/game.js
@@ -37,7 +37,58 @@ function selectGameField(event) {
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
 
-  console.log(gameData);
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
 
+  currentRound++;
   switchPayer();
+}
+
+function checkForGameOver() {
+  // Checking the rows for equality
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  //   Checking the columns for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  //   Checking for Diagonal: Top left to Bottom Right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  //   Checking for Diagonal: Bottom left to Top Right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9) {
+    return -1;
+  }
+
+  return 0;
 }
